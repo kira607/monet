1b2e6e8cc12888a1b4e19059fa9ac24645a873a0
kira607 <kirill.lesckin@gmail.com>
2021-07-30 18:39:09 +0300

    test_run

--- budget/transaction.py
+++ budget/transaction.py
@@ -1,41 +1,60 @@
+from typing import Tuple
+
 from .asset import Asset
 from datetime import datetime
 import random
 import string
 
+
 def gen_transaction_id(prefix='T-', postfix=''):
     id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
     return prefix + id + postfix
 
+
 class Transaction:
     transaction_id = gen_transaction_id()
     asset_from_id: Asset = None
-    asset_to: Asset = None # category
+    asset_to_id: Asset = None  # category
     value: float = 0.0
     pay_date: datetime = None
     plan_date = None
     plan_interval = None
-    transaction_direction = None # income, outcome, internal
-    trasaction_type = None # planned, unplanned, ...
+    transaction_direction = None  # income, outcome, internal
+    transaction_type = None  # planned, unplanned, ...
 
     def __init__(self, from_, to, value, date):
         self.asset_from_ = from_
         self.asset_to = to
         self.value = value
-        self.date = date
+        self.pay_date = date
 
+    @property
     def dict(self):
         return {
             'transaction_id': self.transaction_id,
             'asset_from_id': self.asset_from_id,
-            'asset_to': self.asset_to,
+            'asset_to_id': self.asset_to,
             'value': self.value,
             'pay_date': self.pay_date,
             'plan_date': self.plan_date,
             'plan_interval': self.plan_interval,
             'transaction_direction': self.transaction_direction,
-            'trasaction_type': self.trasaction_type,
+            'transaction_type': self.transaction_type,
         }
+
+    @property
+    def list(self):
+        return [
+            self.transaction_id,
+            self.asset_from_id,
+            self.asset_to,
+            self.value,
+            self.pay_date,
+            self.plan_date,
+            self.plan_interval,
+            self.transaction_direction,
+            self.transaction_type,
+        ]
 
     '''
     {
--- test_run.py
+++ test_run.py
@@ -1,9 +1,50 @@
 import os
 from datetime import datetime
+import pandas as pd
+from typing import List
 
-from budget import CsvStorage
+from pandas import DataFrame
+
+from budget.storage.common.storage import Storage
 from budget.transaction import Transaction
 
-path_to_storage = os.path.join(os.getcwd(), '.data/transactions.csv')
-storage = CsvStorage(path_to_storage)
-storage.add(Transaction('1', '2', 3, datetime.now()))
+
+class TestCsvStorage(Storage):
+    df = None
+
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.__load()
+        super().__init__()
+
+    def add(self, transaction: Transaction):
+        data = transaction.dict
+        index = data['transaction_id']
+        del data['transaction_id']
+        transaction_data_frame = DataFrame(data=data, index=[index])
+        self.df = self.df.append(transaction_data_frame)
+        self.__save()
+
+    def delete(self, *, transaction_id: str = None, filters: List[filter] = None):
+        self.df = self.df.drop(index=transaction_id)
+        self.__save()
+
+    def get(self, *, filters: List[filter] = None):
+        print(self.df[self.df.transaction_type != float('nan')])
+        # for d in self.df.iterrows():
+        #     print(d, type(d), len(d), d[0], d[1], type(d[1]), sep='\n\n')
+        #     break
+
+    def __load(self):
+        self.df = pd.read_csv(self.file_path, index_col=0)
+
+    def __save(self):
+        self.df.to_csv(self.file_path)
+
+
+path_to_storage = os.path.join(os.getcwd(), '.data/test.csv')
+storage = TestCsvStorage(path_to_storage)
+# storage.add(Transaction('1', '2', 3, datetime.now()))
+print(dict(Transaction('1', '2', 3, datetime.now())))
+# storage.delete(transaction_id='T-1IxIed')
+storage.get()
