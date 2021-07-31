e4b2f8a0719129a750532c1b6f3ef5581610b0fc
kira607 <kirill.lesckin@gmail.com>
2021-07-31 18:37:06 +0300

    changes

--- budget/transaction.py
+++ budget/transaction.py
@@ -1,70 +1,57 @@
-from typing import Tuple
+from datetime import datetime
 
 from .asset import Asset
-from datetime import datetime
-import random
-import string
+from .common.utils import gen_id
 
 
-def gen_transaction_id(prefix='T-', postfix=''):
-    id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-    return prefix + id + postfix
+transaction_conf = {
+    'transaction_id': str,
+    'asset_from_id': str,
+    'asset_to_id': str,
+    'value': float,
+    'plan_value': float,
+    'pay_date': datetime,
+    'start_date': datetime,
+    'due_date': datetime,
+}
 
 
 class Transaction:
-    transaction_id = gen_transaction_id()
+    transaction_id = gen_id('T-')
     asset_from_id: Asset = None
     asset_to_id: Asset = None  # category
     value: float = 0.0
     pay_date: datetime = None
-    plan_date = None
-    plan_interval = None
-    transaction_direction = None  # income, outcome, internal
-    transaction_type = None  # planned, unplanned, ...
+    start_date = None
+    due_date = None
 
     def __init__(self, from_, to, value, date):
-        self.asset_from_ = from_
-        self.asset_to = to
+        self.asset_from_id = from_
+        self.asset_to_id = to
         self.value = value
         self.pay_date = date
+        setattr(self, 'plan_value', 4)
 
     @property
     def dict(self):
         return {
             'transaction_id': self.transaction_id,
             'asset_from_id': self.asset_from_id,
-            'asset_to_id': self.asset_to,
+            'asset_to_id': self.asset_to_id,
             'value': self.value,
             'pay_date': self.pay_date,
-            'plan_date': self.plan_date,
-            'plan_interval': self.plan_interval,
-            'transaction_direction': self.transaction_direction,
-            'transaction_type': self.transaction_type,
+            'start_date': self.start_date,
+            'due_date': self.due_date,
         }
 
     @property
     def list(self):
-        return [
+        return (
             self.transaction_id,
             self.asset_from_id,
-            self.asset_to,
+            self.asset_to_id,
             self.value,
             self.pay_date,
-            self.plan_date,
-            self.plan_interval,
-            self.transaction_direction,
-            self.transaction_type,
-        ]
-
-    '''
-    {
-    'name': '',
-    'permanent': '',
-    'recurring type': '',
-    'plan date': '',
-    'plan value': '',
-    'fact value': '',
-    'category': '',
-    'tag': '',
-    }
-    '''
+            self.start_date,
+            self.due_date,
+        )
