30380e66744854fecb402458e0dd85c5ec85c82d
kira607 <kirill.lesckin@gmail.com>
2021-08-24 11:29:18 +0300

    CU-wkz2gt: Described endpoint interface

--- budget/endpoint/common/__base_endpoint.py
+++ budget/endpoint/common/__base_endpoint.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import List
-from budget.models import Transaction
+from typing import Type, Any, Iterable
+
+from budget.models import BaseModel
 
 
 class BaseEndpoint(ABC):
@@ -8,17 +9,44 @@ class BaseEndpoint(ABC):
         pass
 
     @abstractmethod
-    def add(self, _item):
+    def insert(self, model: Type[BaseModel], data: Any) -> BaseModel:
+        '''
+        Insert new model into storage
+
+        :param model: type of model (to identify table)
+        :param data: data to insert (one instance at a call)
+        :return: inserted model instance
+        '''
         pass
 
     @abstractmethod
-    def delete(self, _id: str = None):
+    def delete(self, model: Type[BaseModel], data: Any) -> None:
+        '''
+        Delete model from storage
+
+        :param model: type of model (to identify table)
+        :param data: data to identify model in the storage
+        :return: None
+        '''
         pass
 
     @abstractmethod
-    def get(self, *, filters: List[filter]):
+    def get(self, model: Type[BaseModel]) -> Iterable[BaseModel]:
+        '''
+        Get all models list from the storage
+
+        :param model: type of model (to identify table)
+        :return:
+        '''
         pass
 
     @abstractmethod
-    def update(self):
+    def update(self, model: Type[BaseModel], data: BaseModel) -> BaseModel:
+        '''
+        Update model in the storage
+
+        :param model: type of model (to identify table)
+        :param data: Updated model instance
+        :return: Updated model
+        '''
         pass
