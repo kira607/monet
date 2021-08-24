1b611a2ef7e18938e8c00e51b4bc16465d043f6a
kira607 <kirill.lesckin@gmail.com>
2021-08-24 11:29:18 +0300

    CU-wkz2gt: Described endpoint interface

--- budget/models/__init__.py
+++ budget/models/__init__.py
@@ -1,6 +1,7 @@
 from .account import Account
 from .category import Category
 from .currency import Currency
+from .model import Model as BaseModel
 from .subcategory import SubCategory
 from .tag import Tag
 from .transaction import Transaction
