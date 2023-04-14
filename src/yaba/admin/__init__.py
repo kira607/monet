01df9b9e9c2c88caa7a079761c855a56ba5a50a5
Kirill <kirill.leskin@partner.manga.com>
2023-04-15 01:25:40 +0200

    add simple admin panel

---
+++ src/yaba/admin/__init__.py
@@ -0,0 +1,10 @@
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from yaba.orm import db
+
+
+admin = Admin()
+
+for model_class in db.Model.__subclasses__():
+    admin.add_view(ModelView(model_class, db.session, endpoint=f'{model_class.__tablename__}_model'))
